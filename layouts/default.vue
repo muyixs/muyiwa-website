d5ce9d0e0456ffd102cbca6e71c7c3092219f6ad
Kolapo Oni <onikolapo@gmail.com>
2021-10-28 08:51:21 +0100

    show overlay while preloading background image

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,13 +1,45 @@
 <template>
   <div class="l-app">
     <Nuxt />
+    <div ref="siteLoader" class="c-siteloader"></div>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {}
+  },
+  mounted() {
+    this.loadNoiseBackground()
+  },
+  methods: {
+    loadNoiseBackground() {
+      const vm = this
+      const img = new Image()
+      img.src = require(`@/assets/images/noise.png`)
+
+      img.onload = () => {
+        vm.$refs.siteLoader.style.display = 'none'
+      }
+    },
+  },
+}
+</script>
 
 <style lang="scss">
 @import '~/assets/scss/main.scss';
 
 .l-app {
   padding: 30px 12%;
+}
+
+.c-siteloader {
+  position: fixed;
+  width: 100vw;
+  height: 100vh;
+  top: 0;
+  left: 0;
+  background: $color-pastel-brown;
 }
 </style>
