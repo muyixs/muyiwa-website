9b2acdcb0ad9fd183d0f4b5687886d0fac867e7c
Kolapo Oni <onikolapo@gmail.com>
2021-12-04 13:38:56 +0100

    add site loader

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,7 +3,12 @@
     <div class="c-app__container">
       <top-nav />
       <Nuxt v-if="showPage" />
-      <div v-else ref="siteLoader" class="c-siteloader" />
+      <div v-else class="c-loader">
+        <span ref="loaderText" class="c-loader__text">
+          Loading assets - {{ counter }}%
+        </span>
+        <div class="c-loader__overlay"></div>
+      </div>
       <base-footer />
     </div>
   </div>
@@ -14,22 +19,50 @@ export default {
   data() {
     return {
       showPage: false,
+      counter: 0,
+      loadedAssets: false,
     }
   },
   mounted() {
-    this.loadNoiseBackground()
+    this.preloadAssets()
+    this.startCounter()
   },
   methods: {
-    loadNoiseBackground() {
+    preloadAssets() {
       const vm = this
       const img = new Image()
       img.src = require(`@/assets/images/noise.png`)
 
       img.onload = () => {
         document.fonts.ready.then(() => {
-          vm.showPage = true
+          vm.loadedAssets = true
         })
       }
+    },
+    startCounter() {
+      const counter = setInterval(() => {
+        if (this.counter !== 80) {
+          this.counter += 20
+        } else if (this.loadedAssets) this.counter += 20
+
+        this.$refs.loaderText.style.setProperty('--scale', this.counter / 100)
+
+        if (this.counter === 100) {
+          clearInterval(counter)
+          setTimeout(() => {
+            this.hideLoader()
+          }, 1000)
+        }
+      }, 700)
+    },
+    hideLoader() {
+      this.$refs.loaderText.style.setProperty('--transform-origin', 'right')
+      this.$refs.loaderText.style.setProperty('--scale', 0)
+      this.$refs.loaderText.style.setProperty('--translate-y', '100%')
+
+      setTimeout(() => {
+        this.showPage = true
+      }, 1800)
     },
   },
 }
@@ -59,12 +92,51 @@ export default {
   }
 }
 
-.c-siteloader {
+.c-loader {
   position: fixed;
   width: 100vw;
   height: 100vh;
   top: 0;
   left: 0;
   background: var(--color-pastel-blue);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+
+  &__overlay {
+    width: 100vw;
+    height: 40px;
+    background-color: var(--bg-color);
+    z-index: 2;
+  }
+
+  &__text {
+    --scale: 0;
+    --transform-origin: left;
+    --translate-y: 0;
+    position: relative;
+    font-size: 2.2rem;
+    font-family: 'Inter', 'Roboto', sans-serif;
+    text-transform: uppercase;
+    font-variant-numeric: tabular-nums;
+    transform: translateY(var(--translate-y));
+    transition: transform 0.8s $easeOutExpo 0.9s;
+
+    &::before {
+      content: '';
+      position: absolute;
+      left: 0;
+      height: 40%;
+      width: 100%;
+      bottom: 0;
+      background-color: $color-blue;
+      z-index: -1;
+      opacity: 0.7;
+      transform-origin: var(--transform-origin);
+      transform: scaleX(var(--scale));
+      transition: transform 0.8s $easeOutExpo;
+    }
+  }
 }
 </style>
