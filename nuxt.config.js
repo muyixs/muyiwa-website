386a00eaae4654f4f86f47ee8015f7a166164c77
Kolapo Oni <onikolapo@gmail.com>
2021-12-11 09:02:57 +0100

    update meta title

--- nuxt.config.js
+++ nuxt.config.js
@@ -10,7 +10,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'Muyiwa Adenaike',
+    title: 'Olumuyiwa Adenaike',
     htmlAttrs: {
       lang: 'en',
     },
