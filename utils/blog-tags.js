a5c1a9df151907cb76bafa360d2ed5485d539188
Kolapo Oni <kolapo.oni@flutterwavego.com>
2022-01-08 22:11:52 +0100

    update tags ordering

--- utils/blog-tags.js
+++ utils/blog-tags.js
@@ -1,16 +1,16 @@
 // Tag titles have to be in title case e.g 'My Identity'
 const tags = [
   {
-    title: 'My Identity',
-    slug: 'my-identity'
+    title: 'Computer Science',
+    slug: 'computer-science'
   },
   {
     title: 'Community',
     slug: 'community'
   },
   {
-    title: 'Computer Science',
-    slug: 'computer-science'
+    title: 'My Identity',
+    slug: 'my-identity'
   },
 ]
 
